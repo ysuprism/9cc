21c4b035d125de98cf3100ec025506ca233efd5c
ysuprism <yasuhiro0317@keio.jp>
2020-05-07 17:11:40 +0000

    addition and subtraction of expressions

--- 9cc.c
+++ 9cc.c
@@ -2,14 +2,34 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
-	if(argc != 2){
-		fprintf(stderr, "引数の個数が正しくありません\n");
-		return 1;
-	}
+  if(argc != 2){
+    fprintf(stderr, "引数の個数が正しくありません\n");
+    return 1;
+  }
 
-	printf(".global main\n");
-	printf("main:\n\t");
-	printf("mov $%d, %%rax\n\t", atoi(argv[1]));
-	printf("ret\n");
-	return 0;
+  char *p = argv[1];
+
+  printf(".global main\n");
+  printf("main:\n\t");
+  printf("mov $%ld, %%rax\n\t", strtol(p, &p, 10));
+
+  while(*p){
+    if (*p == '+'){
+      p++;
+      printf("add $%ld, %%rax\n\t", strtol(p, &p, 10));
+      continue;
+    }
+
+    if(*p == '-'){
+      p++;
+      printf("sub $%ld, %%rax\n\t", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
+  printf("ret\n");
+  return 0;
 }
